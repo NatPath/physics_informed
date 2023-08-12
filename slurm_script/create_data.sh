63a0003be947e864fc8af2ceafcb0e7992685ab1
DorShacham <dor.shacham@gmail.com>
2023-08-12 14:43:30 +0300

    create data + pino1000 update

--- slurm_script/create_data.sh
+++ slurm_script/create_data.sh
@@ -5,9 +5,10 @@
 #SBATCH --job-name create_data
 #SBATCH --mail-user=dor-hay.sha@campus.technion.ac.il
 #SBATCH --mail-type=ALL           # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
-#SBATCH --output /home/dor-hay.sha/project/physics_informed/slurm_script/output/create-result-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
+#SBATCH --output /home/dor-hay.sha/project/physics_informed/slurm_script/output/create_dada-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
-#SBATCH --time=0-1:00:00  ## time for analysis (day-hour:min:sec)
+#SBATCH -c 3          # Request 3 cpu
+#SBATCH --time=0-0:2:00  ## time for analysis (day-hour:min:sec)
 
 ##Load the CUDA module
 module load cuda
@@ -17,5 +18,5 @@ conda activate pino-env
 
 ## Run the script
 nvidia-smi
-python SPDC_solver/create_data.py -N 10 --change_pump
+python SPDC_solver/create_data.py -N 100 --change_pump --spp 10
 echo Done
--- slurm_script/pino-1000.sh
+++ slurm_script/pino-1000.sh
@@ -8,6 +8,7 @@
 #SBATCH --output /home/dor-hay.sha/project/physics_informed/slurm_script/output/pino-1000-result-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
 #SBATCH --gres=gpu:A40:1          # Request 1 gpu type A40
+#SBATCH --exclude=nlp-a40-1          # Do not use this gpu becasue it doesnt have enough memory
 #SBATCH --time=10-23:10:00  ## time for analysis (day-hour:min:sec)
 
 ##Load the CUDA module
@@ -18,5 +19,5 @@ conda activate pino-env
 
 ## Run the script
 nvidia-smi
-python train_spdc.py --config_path configs/ngc/SPDC-pino-1000.yaml --mode train
+python train_spdc.py --config_path configs/ngc/SPDC-pino-1000.yaml --mode train --log --validate
 echo Done
