005f7e7f448f4bd6147af74c358a7684fe1692df
DorShacham <dor.shacham@gmail.com>
2023-08-29 10:34:22 +0300

    gitignore

--- slurm_script/pino-tmp.sh
+++ slurm_script/pino-tmp.sh
@@ -2,10 +2,10 @@
 
 ##Resource Request
 
-#SBATCH --job-name pino-tmp
+#SBATCH --job-name pino-r2
 #SBATCH --mail-user=dor-hay.sha@campus.technion.ac.il
 #SBATCH --mail-type=ALL           # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
-#SBATCH --output /home/dor-hay.sha/project/physics_informed/slurm_script/output/pino-100-loss_ratio-4.yaml-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
+#SBATCH --output /home/dor-hay.sha/project/physics_informed/slurm_script/output/SPDC-pino-100-loss_ratio-2-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
 #SBATCH --gres=gpu:A40:1          # Request 1 gpu type A40
 #SBATCH --exclude=nlp-a40-1          # Do not use this gpu becasue it doesnt have enough memory
@@ -19,5 +19,5 @@ conda activate pino-env
 
 ## Run the script
 nvidia-smi
-python train_spdc.py --config_path configs/ngc/SPDC-pino-100-loss_ratio-inf.yaml --mode train --log --validate
+python train_spdc.py --config_path configs/ngc/SPDC-pino-100-loss_ratio-2.yaml --mode train --log --validate
 echo Done
