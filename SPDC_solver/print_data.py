f64eddc02360aed901ab247d83d31104b8a254db
DorShacham <dor.shacham@gmail.com>
2023-09-05 21:14:44 +0300

    add phase to print

--- SPDC_solver/print_data.py
+++ SPDC_solver/print_data.py
@@ -2,13 +2,15 @@ from utils import Shape
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import plotly.express as px
+import plotly
 import pickle
 
-# datapath = "/home/dor-hay.sha/project/data/spdc/random_pump_N-100_spp-1.bin"
-datapath = "./random_pump_N-12_spp-4.bin"
-N = 4
-spp = 1
-z0 = 9
+datapath = "/home/dor-hay.sha/project/data/spdc/fixed_pump_N-500_seed-1701.bin"
+# datapath = "./random_pump_N-12_spp-4.bin"
+N = 0
+spp = 500
+z0 = 0
 
 with open(file=datapath,mode="rb") as file:
     data = pickle.load(file)
@@ -18,18 +20,33 @@ shape = Shape()
 
 X,Y = np.meshgrid(shape.x,shape.y, indexing='ij')
 for z in range(z0,10):
-    for i in range(1):
+    for i in range(5):
+         if "vac" not in dict[i]:
             if True:
-                        fig, ax = plt.subplots(dpi=150,subplot_kw={"projection": "3d"})
-                        if spp==1:
-                               I = np.abs(data["fields"][N,i,:,:,z])**2
-                        else:
-                               I = np.mean(np.abs(data["fields"][N:N+spp,i,:,:,z])**2,axis=0)
-                               
-                        surf = ax.plot_surface(X, Y, I, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-                        fig.colorbar(surf, shrink=0.5, aspect=5)
-                        plt.title(f"{dict[i]}")
-                        plt.savefig(f"{dict[i]}_z={z}.jpg")
-                        plt.close()
+                     fig, (ax1,ax2) = plt.subplots(1,2)
+                     plt.suptitle(f"{dict[i]}_z={z}")
+
+                     if spp==1:
+                              I = np.abs(data["fields"][N,i,:,:,z])**2
+                              phase = np.angle(data["fields"][N,i,:,:,z]) 
+                     else:
+                              I = np.mean(np.abs(data["fields"][N:N+spp,i,:,:,z])**2,axis=0)
+                              phase = np.mean(np.angle(data["fields"][N:N+spp,i,:,:,z]),axis=0)
+                              
+                     im1 = ax1.matshow(I,cmap='coolwarm')
+                     ax1.set_title("Intensity")
+                     fig.colorbar(im1, ax=ax1)
+                     ax1.set_xticks([])
+                     ax1.set_yticks([])
+
+                     im2 = ax2.imshow(phase,cmap='coolwarm')
+                     ax2.set_title("phase [rad]")
+                     fig.colorbar(im2, ax=ax2)
+                     ax2.set_xticks([])
+                     ax2.set_yticks([])
+
+                     fig.tight_layout()
+                     plt.savefig(f"{dict[i]}_z={z}.jpg")
+                     plt.close()
 
 print("Done!")
