cd69b1716b124562c4b0d0939b68b22dfe0f642f
DorShacham <dor.shacham@gmail.com>
2023-09-12 20:20:35 +0300

    adding corr calc

--- correlation_utiles/corr_calc.py
+++ correlation_utiles/corr_calc.py
@@ -1,12 +1,9 @@
 from abc import ABC
-import jax.random as random
 import jax.numpy as np
 
-from spdc_inv.models.utils import Field
-from spdc_inv.models.utils import crystal_prop, propagate
-from spdc_inv.utils.defaults import QUBIT
-from spdc_inv.utils.utils import DensMat
-from spdc_inv.training.utils import (
+from defaults import QUBIT
+from utils import DensMat
+from utils import (
     projection_matrix_calc, projection_matrices_calc,
     decompose, fix_power, get_qubit_density_matrix, get_qutrit_density_matrix,
     coupling_inefficiency_calc_G2, coupling_inefficiency_calc_tomo,
@@ -26,13 +23,9 @@ class SPDCmodel(ABC):
             projection_coincidence_rate,
             projection_tomography_matrix,
             interaction,
-            pump_structure,
-            crystal_hologram,
-            poling_period,
-            DeltaZ,
-            coincidence_rate_observable,
-            density_matrix_observable,
-            tomography_matrix_observable,
+            coincidence_rate_observable: bool = True,
+            density_matrix_observable: bool = True,
+            tomography_matrix_observable: bool = False,
             coupling_inefficiencies: bool = False,
     ):
 
@@ -42,10 +35,6 @@ class SPDCmodel(ABC):
         self.projection_coincidence_rate = projection_coincidence_rate
         self.projection_tomography_matrix = projection_tomography_matrix
         self.interaction = interaction
-        self.pump_structure = pump_structure
-        self.crystal_hologram = crystal_hologram
-        self.poling_period = poling_period
-        self.DeltaZ = DeltaZ
         self.coincidence_rate_observable = coincidence_rate_observable
         self.density_matrix_observable = density_matrix_observable
         self.tomography_matrix_observable = tomography_matrix_observable
@@ -54,88 +43,10 @@ class SPDCmodel(ABC):
 
         self.N = None
         self.N_device = None
-        self.learn_mode = None
-
-        self.signal_f = Field(signal, interaction.dx, interaction.dy, interaction.maxZ)
-        self.idler_f  = Field(idler, interaction.dx, interaction.dy, interaction.maxZ)
 
 
-    def forward(
-            self,
-            model_parameters,
-            rand_key
-    ):
-        pump_coeffs_real, \
-        pump_coeffs_imag, \
-        waist_pump, \
-        crystal_coeffs_real, \
-        crystal_coeffs_imag, \
-        r_scale = model_parameters
 
-        self.pump_structure.create_profile(pump_coeffs_real, pump_coeffs_imag, waist_pump)
-        if self.crystal_hologram is not None:
-            self.crystal_hologram.create_profile(crystal_coeffs_real, crystal_coeffs_imag, r_scale)
-
-        rand_key, subkey = random.split(rand_key)
-        # initialize the vacuum and interaction fields
-        vacuum_states = random.normal(
-            subkey,
-            (self.N_device, 2, 2, self.interaction.Nx, self.interaction.Ny)
-        )
-        signal_out,\
-        idler_out,\
-        idler_vac \
-            = crystal_prop(self.pump_structure.E,
-                           self.pump,
-                           self.signal_f,
-                           self.idler_f,
-                           vacuum_states,
-                           self.interaction,
-                           self.poling_period,
-                           self.N_device,
-                           None if self.crystal_hologram is None else self.crystal_hologram.crystal_profile,
-                           True if not self.learn_mode else None,
-                           signal_init=None,
-                           idler_init=None
-                           )
-
-        # Propagate generated fields back to the middle of the crystal
-        signal_out_back_prop = propagate(signal_out,
-                                         self.interaction.x,
-                                         self.interaction.y,
-                                         self.signal_f.k,
-                               self.DeltaZ
-                               ) * np.exp(-1j * self.signal_f.k * self.DeltaZ)
-
-        idler_out_back_prop  = propagate(idler_out,
-                               self.interaction.x,
-                               self.interaction.y,
-                               self.idler_f.k,
-                               self.DeltaZ
-                               ) * np.exp(-1j * self.idler_f.k * self.DeltaZ)
-
-        idler_vac_back_prop  = propagate(idler_vac,
-                               self.interaction.x,
-                               self.interaction.y,
-                               self.idler_f.k,
-                                        self.DeltaZ
-                                        ) * np.exp(-1j * self.idler_f.k * self.DeltaZ)
-
-        coincidence_rate_projections, tomography_matrix_projections = \
-            self.get_1st_order_projections(
-                signal_out,
-                idler_out,
-                idler_vac,
-                signal_out_back_prop,
-                idler_out_back_prop,
-                idler_vac_back_prop,
-
-
-            )
-
-        observables = self.get_observables(coincidence_rate_projections, tomography_matrix_projections)
-
-        return observables
+    
 
     def get_1st_order_projections(
             self,
@@ -291,10 +202,18 @@ class SPDCmodel(ABC):
 
     def get_observables(
             self,
-            coincidence_rate_projections,
-            tomography_matrix_projections,
-
     ):
+        coincidence_rate_projections, tomography_matrix_projections = \
+            self.get_1st_order_projections(
+                signal_out,
+                idler_out,
+                idler_vac,
+                signal_out_back_prop,
+                idler_out_back_prop,
+                idler_vac_back_prop,
+
+
+            )
         coincidence_rate, density_matrix, tomography_matrix = None, None, None
 
         if self.coincidence_rate_observable:
