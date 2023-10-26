3aa05e2819423f135c9929af13ccca8b14a14986
DorShacham <dor.shacham@gmail.com>
2023-10-27 00:58:41 +0300

    plots for report function was wirtten

--- draw_utils.py
+++ draw_utils.py
@@ -1,7 +1,9 @@
 import cv2
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import os
+from correlation_utiles.draw_corr_utils import total_variation_distance
 
 def image_to_signature(image):
     x= image.shape[0]
@@ -61,38 +63,59 @@ def plot_4_report(u,y,z=9,ckpt_name='default_ckpt.pt',results_dir='default_dir_n
     y = y.reshape(N,nx, ny, nz,2,y_nfields//2)[...,-2:]
     u = (u[...,0,:] + 1j*u[...,1,:]).detach().numpy()
     y = (y[...,0,:] + 1j*y[...,1,:]).detach().numpy()
+    
+    normelize = lambda x: (np.mean(np.abs(x)**2,axis=0) / np.sum(np.mean(np.abs(x)**2,axis=0)))
+    fields = [[normelize(u[...,z,0]),normelize(u[...,z,1])],[normelize(y[...,z,0]),normelize(y[...,z,1])]]
+
 
     dict = {0:"signal out", 1:"idler out"}
     maxXY = 120e-6
     XY = u.shape[1]
     xy = np.linspace(-maxXY, maxXY, XY + 1)[:-1]
     X,Y = np.meshgrid(xy,xy)
-    fig, ax = plt.subplots(2,2,dpi=200,figsize=[10,10])
+    fig, ax = plt.subplots(2,2,dpi=200,figsize=[10,10],subplot_kw={"projection": "3d"})
 
-    for j in range(2):
-        for i,sol,src in zip(range(2),[u,y],["Prediction", "Ground truth"]):
+    for j in range(2): # signal, idler
+        for i,src in zip(range(2),["Prediction", "Ground truth"]):
             # 
             plt.subplot(2,2,i+1+2*j)
             if j==0:
                 ax[0][i].set_title(src)
-            pic = np.mean(np.abs(sol[...,z,j])**2,axis=0) 
-            pic = pic / np.sum(pic)
+            pic = fields[i][j]
+            vmax = np.max(fields[1][j])
 
-            # loc = range(coincidence_rate.shape[0])
-            # ticks = [str(t)  for t in range(-max_mode2,max_mode2+1)]
-            vmax = np.mean(np.abs(y[...,z,0])**2,axis=0) 
-            vmax = np.max(vmax / np.sum(vmax))
+            # im = plt.imshow(pic,vmin=0,vmax=vmax) 
+            surf = ax[j][i].plot_surface(X, Y, pic,linewidth=0, antialiased=False, cmap=cm.Spectral,vmin=0,vmax=vmax)
 
-            im = plt.imshow(pic,vmin=0,vmax=vmax) 
             # plt.xlabel(r'signal mode i')
             # plt.ylabel(r'idle mode j')
             # plt.xticks(loc,ticks)
             # plt.yticks(loc,ticks)
-            plt.colorbar(im)
+            plt.colorbar(surf, shrink=0.5, aspect=10,pad=0.15)
 
-    ax[0][0].text(-3.5,1,"(a)",fontsize=14)
-    ax[1][0].text(-3.5,1,"(b)",fontsize=14)
+    ax[0][0].text(x=-0.00045,y=0,z=0,s="Signal",fontsize=14)
+    ax[1][0].text(x=-0.00045,y=0,z=0,s="Idler",fontsize=14)
 
     plt.figure(fig)
     plt.savefig(f"{results_dir}/fields.jpg")
     plt.close('all')
+
+    # calculting errors
+    with open(str(f"{results_dir}/fields_err.txt"),"w") as file:
+                signal_pred = fields[0][0]
+                signal_grt = fields[1][0]
+                mse_err = ((signal_pred-signal_grt)**2).mean()
+                file.write(f"Signal out mse error:{mse_err}\n")
+                
+                tvd_err = total_variation_distance(signal_pred,signal_grt)
+                file.write(f"Signal out Total variation distance:{tvd_err}\n")
+
+
+                file.write(f"--------\n")
+                idler_pred = fields[0][1]
+                idler_grt = fields[1][1]
+                mse_err = ((idler_pred-idler_grt)**2).mean()
+                file.write(f"Idler out mse error:{mse_err}\n")
+                
+                tvd_err = total_variation_distance(idler_pred,idler_grt)
+                file.write(f"Idler out Total variation distance:{tvd_err}\n")
